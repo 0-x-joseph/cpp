d4744730355d4d5bbe4d024bcccf526890a6e9e5
Youssef Bouryal <youssefbouryal02@gmail.com>
2025-08-13 00:26:53 +0100

    undone

---
+++ ex01/Contact.hpp
@@ -0,0 +1,13 @@
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+class Contact {
+public:
+	char	*first_name;
+	char	*last_name;
+	char	*nickname;
+	char	*phone_number;
+	char	*secret;
+};
+
+#endif
---
+++ ex01/PhoneBook.hpp
@@ -0,0 +1,21 @@
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+class PhoneBook {
+private:
+
+public:
+
+	PhoneBook(void);
+
+	void	get_values;
+
+	void	add(char *fname, char *lname, char *nickname, char *phone_number, char *secret);
+
+
+
+
+	~PhoneBook(void);
+};
+
+#endif /* PHONEBOOK_HPP */
